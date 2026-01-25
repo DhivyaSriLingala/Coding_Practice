4f7c98052380b3e1ee0eec7078180c385c8c3c82
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2026-01-25 17:36:35 -0500

    Refactor Two Sum solution to Python implementation

--- LeetCode/Two-Sum/Solution.py
+++ LeetCode/Two-Sum/Solution.py
@@ -1,12 +1,7 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
-    }
-  }
- }
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j]+nums[i]==target:
+                    return [i,j]
+        return []
