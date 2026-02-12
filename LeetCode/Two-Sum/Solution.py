00729a0703088eb3018225051d4c90c98c4f6978
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2026-02-12 13:30:18 -0500

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.py
+++ LeetCode/Two-Sum/Solution.py
@@ -1,9 +1,7 @@
 1class Solution:
 2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        prevMap={}
-4        for i,n in enumerate(nums):
-5            diff=target-n
-6            if diff in prevMap:
-7                return [prevMap[diff],i]
-8            prevMap[n]=i
-9        return
+3        for i in range(len(nums)):
+4            for j in range(i+1,len(nums)):
+5                if nums[j]+nums[i]==target:
+6                    return [i,j]
+7        return []
