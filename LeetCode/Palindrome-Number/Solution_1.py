6680d540714731840ef1102f11fd81af78986bcf
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2026-02-19 15:30:44 -0500

    Add palindrome number solution in Python

---
+++ LeetCode/Palindrome-Number/Solution_1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num=x
+        rev=0
+        while x>0:
+            rev=rev*10+(x%10)
+            x=x//10
+        if num==rev:
+            return True
+        else:
+            return False  
