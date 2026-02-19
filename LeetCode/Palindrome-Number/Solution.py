e4f2d09b7d3789e2444b65a75ecb84a1bf3f3f4b
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2026-02-19 15:29:05 -0500

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.py
+++ LeetCode/Palindrome-Number/Solution.py
@@ -1,11 +1,8 @@
 1class Solution:
 2    def isPalindrome(self, x: int) -> bool:
-3        num=x
-4        rev=0
-5        while x>0:
-6            rev=rev*10+(x%10)
-7            x=x//10
-8        if num==rev:
-9            return True
-10        else:
-11            return False  
+3        x=str(x)
+4        n=len(x)
+5        for i in range(0,n//2):
+6            if x[i]!=x[n-i-1]:
+7                return False
+8        return True
